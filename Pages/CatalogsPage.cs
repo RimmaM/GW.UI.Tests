26a0d7d6e124ec6d061f4293ffd5f7aafa47f868
RimmaM <rimma.maksimova@gmail.com>
2026-07-21 20:24:38 +0300

    commit 2 files CatalogsTests/CatalogsPage

---
+++ Pages/CatalogsPage.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Playwright;
+
+namespace GW.UI.Tests.Pages;
+
+/// <summary>
+/// Страница "Сотрудники"
+/// </summary>
+public class CatalogsPage
+{
+    private readonly IPage _page; // Ссылка на открытую страницу браузера
+
+    public CatalogsPage(IPage page) // Конструктор страницы
+    {
+        _page = page;
+    }
+
+    private ILocator SearchField => _page.Locator("input[data-searchfield]");                          // Поле поиска сотрудников
+    private ILocator StatusSelector => _page.Locator(".selector-field");                               // Выпадающий список "Статус"
+    private ILocator ConfirmedStatus => _page.Locator(".option-item[data-id='Confirmed']");            // Пункт "Подтвержден"
+    private ILocator ResetButton => _page.GetByRole(AriaRole.Button, new() { Name = "Сбросить все" }); // Кнопка "Сбросить все"
+
+    /// <summary>
+    /// Открывает страницу сотрудников
+    /// </summary>
+    public async Task Open()
+    {
+        await _page.GotoAsync("https://gw-exp.dev.artintech.ru/Catalogs"); // Переходим на страницу сотрудников
+        //await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);           // Ждем полной загрузки страницы
+        await SearchField.WaitForAsync();                                  // Ждем появления поля поиска
+    }
+
+    /// <summary>
+    /// Выбирает статус "Подтвержден"
+    /// </summary>
+    public async Task SelectConfirmedStatus()
+    {
+        await StatusSelector.ClickAsync();                           // Открываем список
+        await ConfirmedStatus.WaitForAsync();                        // Ждем появления пункта "Подтвержден"
+        await ConfirmedStatus.ClickAsync();                          // Выбираем "Подтвержден"
+    }
+
+    /// <summary>
+    /// Сбрасывает все фильтры
+    /// </summary>
+    public async Task ResetFilters()
+    {
+        await ResetButton.ClickAsync();                                     // Нажимаем кнопку "Сбросить все"
+        //await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);           // Ждем обновления списка
+        await SearchField.WaitForAsync();      // Ждем, пока поле поиска снова станет доступным
+        Console.WriteLine(await SearchField.InputValueAsync());
+    }
+
+    /// <summary>
+    /// Выполняет поиск сотрудника
+    /// </summary>
+    /// <param name="employeeName">Фамилия сотрудника</param>
+    public async Task SearchEmployee(string employeeName)
+    {
+        await SearchField.ClearAsync();                                     // Очищаем поле поиска
+        await SearchField.FillAsync(employeeName);                          // Вводим фамилию сотрудника
+       // await SearchField.PressAsync("Enter");                              // Запускаем поиск
+        //await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);           // Ждем обновления списка
+        await _page.WaitForTimeoutAsync(1000);     // Даем странице обновить список
+    }
+
+
+    /// <summary>
+    /// Открывает карточку сотрудника по фамилии
+    /// </summary>
+    /// <param name="lastName">Фамилия сотрудника</param>
+    public async Task OpenEmployee(string lastName)
+    {
+        var employee = _page.Locator(".list-item")                                   // Находим все строки списка сотрудников
+            .Filter(new() { HasText = lastName })                                    // Оставляем строку с нужной фамилией
+            .Locator("a[href*='EmployeeView']");                                     // Находим ссылку с номером сотрудника
+
+        await employee.ClickAsync();                                                 // Открываем карточку сотрудника
+        await _page.WaitForURLAsync("**/Catalogs/EmployeeView/**");                  // Ждем открытия страницы сотрудника
+    }
+
+    /// <summary>
+    /// Возвращает значение поля поиска
+    /// </summary>
+    public async Task<string> GetSearchValue()
+    {
+        return await SearchField.InputValueAsync(); // Получаем текст из поля поиска
+    }
+
+    /// <summary>
+    /// Возвращает текущий адрес страницы
+    /// </summary>
+    public string GetCurrentUrl()
+    {
+        return _page.Url; // Возвращаем адрес открытой страницы
+    }
+
+
+
+}
---
+++ Tests/CatalogsTests.cs
@@ -0,0 +1,60 @@
+﻿using GW.UI.Tests.Pages;
+using GW.UI.Tests.TestData;
+using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace GW.UI.Tests.Tests;
+
+[TestFixture]
+public class CatalogsTests
+{
+    private IPlaywright _playwright;      // Экземпляр Playwright
+    private IBrowser _browser;            // Экземпляр браузера
+    private IPage _page;                  // Открытая вкладка браузера
+
+    private LoginPage _loginPage;         // Страница авторизации
+    private CatalogsPage _catalogsPage;   // Страница сотрудников
+
+    [SetUp]
+    public async Task SetUp()
+    {
+        _playwright = await Playwright.CreateAsync();                                              // Запускаем Playwright
+        _browser = await _playwright.Chromium.LaunchAsync(new() { Headless = false });             // Запускаем браузер
+        _page = await _browser.NewPageAsync();                                                      // Открываем новую вкладку
+
+        _loginPage = new LoginPage(_page);                                                         // Создаем объект страницы авторизации
+        _catalogsPage = new CatalogsPage(_page);                                                   // Создаем объект страницы сотрудников
+    }
+
+    [TearDown]
+    public async Task TearDown()
+    {
+        await _browser.CloseAsync();                                                               // Закрываем браузер
+        _playwright.Dispose();                                                                     // Освобождаем ресурсы Playwright
+    }
+
+    [Test]
+    public async Task Search_Confirmed_Employee()
+    {
+        await _loginPage.Open();                                                                   // Открываем страницу авторизации
+        await _loginPage.Login(Users.Email, Users.Password);                                      // Выполняем вход
+
+        await _page.WaitForURLAsync("**/Travels");                                                 // Ждем успешной авторизации
+
+        await _catalogsPage.Open();                                                                // Переходим на страницу сотрудников
+
+        await _catalogsPage.SelectConfirmedStatus();                                               // Выбираем статус "Подтвержден"
+
+        await _catalogsPage.ResetFilters();                                                        // Сбрасываем фильтры
+
+        Assert.That(await _catalogsPage.GetSearchValue(), Is.EqualTo(""));                         // Проверяем, что поле поиска очищено
+
+        await _catalogsPage.SearchEmployee("Родионова");                                           // Ищем сотрудника Родионова
+
+        await _catalogsPage.OpenEmployee("Родионова");                                             // Открываем карточку сотрудника
+
+        Assert.That(
+            _catalogsPage.GetCurrentUrl(),
+            Does.Contain("/Catalogs/EmployeeView/"));                                              // Проверяем, что открылась карточка сотрудника
+    }
+}
